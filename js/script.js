4a25395056653cc03ba20d25e90119a64dd82f68
Dmitry Nogochev <webmasterdima@yandex.ru>
2019-10-10 13:45:39 +0300

    Добавлены состояния полей ввода

--- js/script.js
+++ js/script.js
@@ -31,20 +31,6 @@ try {
   close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
-  });
-
-  form.addEventListener("submit", function (evt) {
-    if (!name.value || !email.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("name", name.value);
-      }
-    }
   });
 
   window.addEventListener("keydown", function (evt) {
@@ -52,7 +38,6 @@ try {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
       }
     }
   });
